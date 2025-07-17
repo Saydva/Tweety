832de825dd5a3d4b8a12383f0a3a0a7432196cff
Saydva <kacurikjozef@gmai.com>
2025-07-17 19:07:45 +0200

    added delete button

--- react-front/src/App.tsx
+++ react-front/src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import AddTweety from "./addTweet/addTweety";
+import AddTweety from "./addTweet/AddTweety";
 
 function App() {
   const [value, setValue] = useState("");
@@ -12,13 +12,29 @@ function App() {
     localStorage.setItem("tweets", JSON.stringify(tweets));
   }, [tweets]);
 
+  const handleDeleteTweet = (index: number) => {
+    setTweets((prevTweets) => {
+      const updatedTweets = [...prevTweets];
+      updatedTweets.splice(index, 1);
+      return updatedTweets;
+    });
+  };
+
   return (
     <div className="App flex flex-col items-baseline justify-center min-h-screen gap-2 w-xl mx-auto">
       <AddTweety value={value} setValue={setValue} setTweets={setTweets} />
       <div className="flex flex-col justify-baseline items-baseline w-full max-w-3xl">
-        <ul>
+        <ul className="flex flex-col items-baseline justify-center w-full max-w-3xl">
           {tweets.map((tweet, index) => (
-            <li key={index}>{tweet}</li>
+            <li
+              key={index}
+              className="flex items-center justify-between p-2 mb-2 rounded shadow w-full"
+            >
+              {tweet}
+              <button className="btn " onClick={() => handleDeleteTweet(index)}>
+                Delete
+              </button>
+            </li>
           ))}
         </ul>
       </div>
